8ee85a43373315047c6cbdbffd744c9b82154155
jtodog <jtodog@outlook.com>
2025-12-02 21:42:41 +0800

    position control mode takeoff success.

--- src/sfc_navigation.cpp
+++ src/sfc_navigation.cpp
@@ -1,5 +1,6 @@
 // sfc_naviigation is a whole task class and ros node for navigation using SFC-based planner
-# include <ros/ros.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -8,6 +9,8 @@
 #include <mavros_msgs/State.h>
 #include <Eigen/Dense>
 #include <thread>
+
+#include <tracking_controller/Target.h>
 
 #include "gcopter/trajectory.hpp"
 #include "gcopter/gcopter.hpp"
@@ -35,7 +38,6 @@ struct Config
         double vertDrag;
         double parasDrag;
         double speedEps;
-        
         double DesHeight;
 
         Config(const ros::NodeHandle &nh_priv)
@@ -66,7 +68,7 @@ class SFCNavigation
     ros::Subscriber mapSub_;
     ros::Subscriber targetSub_;
     ros::Subscriber mavros_stateSub_;
-    ros::Subcriber odomSub_;
+    ros::Subscriber odomSub_;
     ros::ServiceClient armClient_;
 	ros::ServiceClient setModeClient_;
     ros::Publisher posePub_;
@@ -80,27 +82,31 @@ class SFCNavigation
     Eigen::Vector3d Goal_; // target position
     Eigen::Vector3d currPos_;
 
+    tracking_controller::Target stateTgt_;
     geometry_msgs::PoseStamped poseTgt_;   // position control target
 
     nav_msgs::Odometry odom_;
     mavros_msgs::State mavrosState_;
 
     Eigen::Vector3d currVel_;
+    Eigen::Vector3d prevVel_;
     Eigen::Vector3d currAcc_;
     ros::Time prevStateTime_;
 
 
 
-    bool mapInitialized_;
-    bool odomReceived_;
-    bool mavrosStateReceived_;
-    bool stateUpdateFirstTime_;
+    bool mapInitialized_ = false;
+    bool odomReceived_ = false;
+    bool mavrosStateReceived_ = false;
+    bool stateUpdateFirstTime_ = true;
+    bool waitForGoal_ = false;
+    bool holdingPosition_ = false;
 
     // Subcriber callback functions
-    inline void mapCB(const sensor_msgs::PointCloud2::ConstPtr &msg)
+    void mapCB(const sensor_msgs::PointCloud2::ConstPtr &msg)
     {
         // Initialize voxel map from point cloud
-        if (!mapInitialized)
+        if (!mapInitialized_)
         {
             size_t cur = 0;
             const size_t total = msg->data.size() / msg->point_step;
@@ -116,25 +122,26 @@ class SFCNavigation
                 }
                 voxelMap_.setOccupied(Eigen::Vector3d(fdata[cur + 0],fdata[cur + 1],fdata[cur + 2]));
             }
-            voxelMap_.dilate(std::ceil(config.dilateRadius / voxelMap.getScale()));
-            mapInitialized = true;
-            ROS__INFO("[sfc_Nav]:Voxel map initialized.");
+            voxelMap_.dilate(std::ceil(config_.dilateRadius / voxelMap_.getScale()));
+            mapInitialized_ = true;
+            ROS_INFO("[sfc_Nav]:Voxel map initialized.");
         }
     }
     
-    inline void targetCB(const geometry_msgs::PoseStamped::ConstPtr &msg)
+    void targetCB(const geometry_msgs::PoseStamped::ConstPtr &msg)
     {
-        if (mapInitialized)
+        if (mapInitialized_)
         {
-            if (Goal.empty())
+            if (waitForGoal_)
             {
                 const double zGoal = config_.DesHeight; // desired height
                 Eigen::Vector3d target(msg->pose.position.x, msg->pose.position.y, zGoal);
                 if (voxelMap_.query(target) == 0)
                 {
                     Goal_ = target;
-                    ROS_INFO("[sfc_Nav]:New target received: (%.2f, %.2f, %.2f).", Goal(0), Goal(1), Goal(2));
-                    plan(); // TODO: implement plan function
+                    ROS_INFO("[sfc_Nav]:New target received: (%.2f, %.2f, %.2f).", Goal_(0), Goal_(1), Goal_(2));
+                    waitForGoal_ = false;
+                    //plan(); // TODO: implement plan function
                 }
                 else
                 {
@@ -150,7 +157,7 @@ class SFCNavigation
         return;
     }
 
-    inline void mavros_stateCB(const mavros_msgs::State::ConstPtr &msg)
+    void mavros_stateCB(const mavros_msgs::State::ConstPtr &msg)
     {
         mavrosState_ = *msg;
 		if (not mavrosStateReceived_){
@@ -158,39 +165,40 @@ class SFCNavigation
 		}
     }
 
-    inline void odomCB(const nav_msgs::Odometry::ConstPtr &msg)
+    void odomCB(const nav_msgs::Odometry::ConstPtr &odom)
     {
         odom_ = *odom;
-		currPos_(0) = this->odom_.pose.pose.position.x;
-		currPos_(1) = this->odom_.pose.pose.position.y;
-		currPos_(2) = this->odom_.pose.pose.position.z;
+		currPos_(0) = odom_.pose.pose.position.x;
+		currPos_(1) = odom_.pose.pose.position.y;
+		currPos_(2) = odom_.pose.pose.position.z;
 		if (not odomReceived_){
 			odomReceived_ = true;
 		}
     }
 
-    inline void stateUpdateCB(const ros::TimerEvent &e)
+    void stateUpdateCB(const ros::TimerEvent &e)
     {
-        Eigen::Vector3d currVelBody (this->odom_.twist.twist.linear.x, this->odom_.twist.twist.linear.y, this->odom_.twist.twist.linear.z);
-		Eigen::Vector4d orientationQuat (this->odom_.pose.pose.orientation.w, this->odom_.pose.pose.orientation.x, this->odom_.pose.pose.orientation.y, this->odom_.pose.pose.orientation.z);
+        Eigen::Vector3d currVelBody (odom_.twist.twist.linear.x, odom_.twist.twist.linear.y, odom_.twist.twist.linear.z);
+		Eigen::Vector4d orientationQuat (odom_.pose.pose.orientation.w, odom_.pose.pose.orientation.x, odom_.pose.pose.orientation.y, odom_.pose.pose.orientation.z);
         Eigen::Matrix3d orientationRot = AutoFlight::quat2RotMatrix(orientationQuat);
-        this->currVel_ = orientationRot * currVelBody; // convert to world frame
+        currVel_ = orientationRot * currVelBody; // convert to world frame
         ros::Time currTime = ros::Time::now();	
-		if (this->stateUpdateFirstTime_){
-			this->currAcc_ = Eigen::Vector3d (0.0, 0.0, 0.0);
-			this->prevStateTime_ = currTime;
-			this->stateUpdateFirstTime_ = false;
+		if (stateUpdateFirstTime_){
+			currAcc_ = Eigen::Vector3d (0.0, 0.0, 0.0);
+			prevStateTime_ = currTime;
+            prevVel_ = currVel_;
+			stateUpdateFirstTime_ = false;
 		}
         else
         {
-            double dt = (currTime - this->prevStateTime_).toSec();
-			this->currAcc_ = (this->currVel_ - this->prevVel_)/dt; 
-			this->prevVel_ = this->currVel_; 
-			this->prevStateTime_ = currTime;
+            double dt = (currTime - prevStateTime_).toSec();
+			currAcc_ = (currVel_ - prevVel_)/dt; 
+			prevVel_ = currVel_; 
+			prevStateTime_ = currTime;
         }
     }
 
-    inline void publishTarget()
+    void publishTarget()
     {
         /*
         isolated thread to publish target state
@@ -199,11 +207,22 @@ class SFCNavigation
 		// warmup
 		for(int i = 100; ros::ok() && i > 0; --i){
             poseTgt_.header.stamp = ros::Time::now();
-            // poseTgt_.pose.position.x = currPos_(0);
-            // poseTgt_.pose.position.y = currPos_(1);
-            // poseTgt_.pose.position.z = currPos_(2);
-            posePub_.publish(poseTgt_); // position control
+            poseTgt_.pose.position.x = currPos_(0);
+            poseTgt_.pose.position.y = currPos_(1);
+            poseTgt_.pose.position.z = currPos_(2);
+            posePub_.publish(poseTgt_); // position control to warm up
         }
+        // initial update stateTgt_
+        tracking_controller::Target psT;
+		psT.type_mask = tracking_controller::Target::IGNORE_ACC_VEL;
+		psT.header.frame_id = "map";
+		psT.header.stamp = ros::Time::now();
+		psT.position.x = currPos_(0);
+		psT.position.y = currPos_(1);
+		psT.position.z = currPos_(2);
+		psT.yaw = AutoFlight::rpy_from_quaternion(odom_.pose.pose.orientation);
+		updateTargetWithState(psT);
+
         // vehicle mode and arm commands
         mavros_msgs::SetMode offboardMode;
 		offboardMode.request.custom_mode = "OFFBOARD";
@@ -212,35 +231,37 @@ class SFCNavigation
         ros::Time lastRequest = ros::Time::now();
         while (ros::ok()){
             //
-			if (this->mavrosState_.mode != "OFFBOARD" && (ros::Time::now() - lastRequest > ros::Duration(5.0))){
-                if (this->setModeClient_.call(offboardMode) && offboardMode.response.mode_sent)
+			if (mavrosState_.mode != "OFFBOARD" && (ros::Time::now() - lastRequest > ros::Duration(5.0))){
+                if (setModeClient_.call(offboardMode) && offboardMode.response.mode_sent)
                 {
                     ROS_INFO("[sfc_Nav]: Offboard enabled.");
                 }
                 lastRequest = ros::Time::now();
             } else {
-                if (!this->mavrosState_.armed && (ros::Time::now() - lastRequest > ros::Duration(5.0)))
+                if (!mavrosState_.armed && (ros::Time::now() - lastRequest > ros::Duration(5.0)))
                 {
-                    if (this->armClient_.call(armCmd) && armCmd.response.success){
+                    if (armClient_.call(armCmd) && armCmd.response.success){
                         ROS_INFO("[sfc_Nav]: Vehicle armed success.");
                     }
                     lastRequest = ros::Time::now();
                 }
             }
+            // position control
+            posePub_.publish(poseTgt_);
             // state control
-			this->statePub_.publish(this->stateTgt_);
+			// statePub_.publish(stateTgt_);
 			r200.sleep();
 		}	
     }
 
-    inline void updateTarget(const geometry_msgs::PoseStamped& position)
+    void updateTarget(const geometry_msgs::PoseStamped& position)
     {
-        this->poseTgt_ = position;
-        this->poseTgt_.header.frame_id = "map";
+        poseTgt_ = position;
+        poseTgt_.header.frame_id = "map";
     }
 
-    void flightBase::updateTargetWithState(const tracking_controller::Target& state){
-		this->stateTgt_ = state;
+    void updateTargetWithState(const tracking_controller::Target& state){
+		stateTgt_ = state;
 	}
 
     public:
@@ -252,7 +273,7 @@ class SFCNavigation
                             (config_.mapBound[3] - config_.mapBound[2]) / config_.voxelWidth,
                             (config_.mapBound[5] - config_.mapBound[4]) / config_.voxelWidth);
     const Eigen::Vector3d offset(config_.mapBound[0], config_.mapBound[2], config_.mapBound[4]);
-    voxelMap = voxel_map::VoxelMap(xyz, offset, config_.voxelWidth);
+    voxelMap_ = voxel_map::VoxelMap(xyz, offset, config_.voxelWidth);
 
     // Subcribers
     mapSub_ = nh_.subscribe(config_.mapTopic, 1, &SFCNavigation::mapCB, this, ros::TransportHints().tcpNoDelay());
@@ -260,18 +281,18 @@ class SFCNavigation
     mavros_stateSub_ = nh_.subscribe<mavros_msgs::State>(config_.mavros_stateTopic, 1000, &SFCNavigation::mavros_stateCB, this);
     odomSub_ = nh_.subscribe<nav_msgs::Odometry>(config_.odomTopic, 1000, &SFCNavigation::odomCB, this);
     // Service client
-    armClient_ = this->nh_.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
-    setModeClient_ = this->nh_.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");	
+    armClient_ = nh_.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    setModeClient_ = nh_.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");	
     // Publishers
-    posePub_ = this->nh_.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 1000);
-	statePub_ = this->nh_.advertise<tracking_controller::Target>("/autonomous_flight/target_state", 1000);
+    posePub_ = nh_.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 1000);
+	statePub_ = nh_.advertise<tracking_controller::Target>("/autonomous_flight/target_state", 1000);
     
     // Wait for odometry and mavros to be ready
-    this->odomReceived_ = false;
-    this->mavrosStateReceived_ = false;
+    odomReceived_ = false;
+    mavrosStateReceived_ = false;
     ROS_INFO("[sfc_Nav]:Waiting for odometry and mavros state...");
     ros::Rate r10(10);
-    while (ros::ok() and not (this->odomReceived_ and this->mavrosStateReceived_)){
+    while (ros::ok() and not (odomReceived_ and mavrosStateReceived_)){
         ros::spinOnce();
         r10.sleep();
     }
@@ -280,13 +301,19 @@ class SFCNavigation
     targetPubWorker_ = std::thread(&SFCNavigation::publishTarget, this);
     targetPubWorker_.detach();
     // Vehicle state update timer
-    stateUpdateTimer_ = this->nh_.createTimer(ros::Duration(0.033), &SFCNavigation::stateUpdateCB, this);
+    stateUpdateTimer_ = nh_.createTimer(ros::Duration(0.033), &SFCNavigation::stateUpdateCB, this);
     }
 
-    inline ~SFCNavigation()
-    {}
 
-    inline void takeoff(){
+    void takeoff(){
+
+        // wait for map initialization
+        ROS_INFO("[sfc_Nav]:takeoff is waiting for map initialization...");
+        ros::Rate r5 (5);
+        while (ros::ok() and not mapInitialized_){
+            ros::spinOnce();
+            r5.sleep();
+        }
         geometry_msgs::PoseStamped ps;
 		ps.header.frame_id = "map";
 		ps.header.stamp = ros::Time::now();
@@ -294,24 +321,29 @@ class SFCNavigation
 		ps.pose.position.y = odom_.pose.pose.position.y;
         ps.pose.position.z = config_.DesHeight;
         ps.pose.orientation = odom_.pose.pose.orientation;
-        updateTarget(ps);
+        updateTarget(ps); // position control target update
 
         tracking_controller::Target psT;
-		psT.type_mask = psT.IGNORE_ACC_VEL;
+		psT.type_mask = tracking_controller::Target::IGNORE_ACC_VEL;
 		psT.header.frame_id = "map";
 		psT.header.stamp = ros::Time::now();
-		psT.position.x = this->odom_.pose.pose.position.x;
-		psT.position.y = this->odom_.pose.pose.position.y;
-		psT.position.z = this->takeoffHgt_;
-		psT.yaw = AutoFlight::rpy_from_quaternion(this->odom_.pose.pose.orientation);
+		psT.position.x = odom_.pose.pose.position.x;
+		psT.position.y = odom_.pose.pose.position.y;
+		psT.position.z = config_.DesHeight;
+		psT.yaw = AutoFlight::rpy_from_quaternion(odom_.pose.pose.orientation);
 		updateTargetWithState(psT);
 
         ROS_INFO("[sfc_Nav]:Start taking off to height %.2f m.", config_.DesHeight);
         ros::Rate r30 (30);
-		while (ros::ok() and std::abs(this->odom_.pose.pose.position.z - this->takeoffHgt_) >= 0.1){
+		while (ros::ok() && std::abs(odom_.pose.pose.position.z - config_.DesHeight) >= 0.05){
+            std::cout << "\r[sfc_Nav]: Current Height: " 
+            << std::fixed << std::setprecision(2) 
+            << odom_.pose.pose.position.z << " (m)"
+            << std::flush;
 			ros::spinOnce();
 			r30.sleep();
 		}
+        std::cout << std::endl;
         ros::Time startTime = ros::Time::now();
         // hold for 3 seconds
 		while (ros::ok()){
@@ -325,8 +357,33 @@ class SFCNavigation
         ROS_INFO("[sfc_Nav]:Takeoff success.");
     }
 
+    void holdPosition(){
+        ROS_INFO("[sfc_Nav]:Holding position.");
+        holdingPosition_ = true;
+        ros::Rate r30 (30);
+        tracking_controller::Target psT;
+		psT.type_mask = tracking_controller::Target::IGNORE_ACC_VEL;
+		psT.header.frame_id = "map";
+		psT.header.stamp = ros::Time::now();
+		psT.position.x = odom_.pose.pose.position.x;
+		psT.position.y = odom_.pose.pose.position.y;
+		psT.position.z = config_.DesHeight;
+		psT.yaw = AutoFlight::rpy_from_quaternion(odom_.pose.pose.orientation);
+		updateTargetWithState(psT);
+        while (ros::ok() && holdingPosition_){
+            std::cout << "\r[sfc_Nav]: Current Height: " 
+            << std::fixed << std::setprecision(2) 
+            << odom_.pose.pose.position.z << " (m)"
+            << std::flush;
+            ros::spinOnce();
+            r30.sleep();
+        }
+        std::cout << std::endl;
+        ROS_INFO("[sfc_Nav]:Stop holding position.");
+    }
+};
 
-}
+    
 
 int main(int argc, char **argv)
 {
